cbc21de0083beb89414f2cc48390d175ed6ae3e1
EmileZ10 <emile.zanna@epfedu.fr>
2026-04-03 16:49:33 +0200

    Remplacement des listes par une bdd SQL model pour stocker les données
    Co-authored-by: JZchallenge <JZchallenge@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,24 +3,34 @@ from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Annotated
+from fastapi import Depends
+
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
-skills = []
-education = []
-professional_experience = []
-languages = []
-info = []
-projects = []
+sqlite_url = "sqlite:///./cv.db"
+engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
 
 
-class Skill(BaseModel):
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+class Skill(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     software: str
     level: str
 
 
-class Education(BaseModel):
+class Education(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     school: str
     degree: str
     field_of_study: str
@@ -28,7 +38,8 @@ class Education(BaseModel):
     end_year: int
 
 
-class ProfessionalExperience(BaseModel):
+class ProfessionalExperience(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     company: str
     position: str
     start_date: int
@@ -36,12 +47,14 @@ class ProfessionalExperience(BaseModel):
     description: str
 
 
-class Language(BaseModel):
+class Language(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     language_name: str
     level: str
 
 
-class Info(BaseModel):
+class Info(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     email: str
     phone: str
     location: str
@@ -49,102 +62,130 @@ class Info(BaseModel):
     github: str
 
 
-class Project(BaseModel):
+class Project(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     name_project: str
     description: str
     link: str
 
 
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
+
+
 @app.post("/skills")
-def create_skill(software: str, level: str):
-    skill = {"software": software, "level": level}
-    skills.append(skill)
-    response = {"id": len(skills) - 1, "skill": skill}
-    return response
+def create_skill(software: str, level: str, session: SessionDep):
+    skill = Skill(software=software, level=level)
+    session.add(skill)
+    session.commit()
+    session.refresh(skill)
+    return skill
 
 
 @app.post("/education")
 def create_education(
-    school: str, degree: str, field_of_study: str, start_year: int, end_year: int
+    school: str,
+    degree: str,
+    field_of_study: str,
+    start_year: int,
+    end_year: int,
+    session: SessionDep,
 ):
-    education_dict = {
-        "school": school,
-        "degree": degree,
-        "field_of_study": field_of_study,
-        "start_year": start_year,
-        "end_year": end_year,
-    }
-    education.append(education_dict)
-    response = {"id": len(education) - 1, "education": education_dict}
-    return response
+    education = Education(
+        school=school,
+        degree=degree,
+        field_of_study=field_of_study,
+        start_year=start_year,
+        end_year=end_year,
+    )
+    session.add(education)
+    session.commit()
+    session.refresh(education)
+    return education
 
 
 @app.post("/professional_experience")
 def create_professional_experience(
-    company: str, position: str, start_date: int, end_date: int, description: str
+    company: str,
+    position: str,
+    start_date: int,
+    end_date: int,
+    description: str,
+    session: SessionDep,
 ):
-    exp_dict = {
-        "company": company,
-        "position": position,
-        "start_date": start_date,
-        "end_date": end_date,
-        "description": description,
-    }
-    professional_experience.append(exp_dict)
-    response = {"id": len(professional_experience) - 1, "experience": exp_dict}
-    return response
+    experience = ProfessionalExperience(
+        company=company,
+        position=position,
+        start_date=start_date,
+        end_date=end_date,
+        description=description,
+    )
+    session.add(experience)
+    session.commit()
+    session.refresh(experience)
+    return experience
 
 
 @app.post("/languages")
-def create_language(language_name: str, level: str):
-    lang_dict = {"language_name": language_name, "level": level}
-    languages.append(lang_dict)
-    response = {"id": len(languages) - 1, "language": lang_dict}
-    return response
+def create_language(language_name: str, level: str, session: SessionDep):
+    language = Language(language_name=language_name, level=level)
+    session.add(language)
+    session.commit()
+    session.refresh(language)
+    return language
 
 
 @app.post("/info")
-def create_info(email: str, phone: str, location: str, linkedin: str, github: str):
-    info_dict = {
-        "email": email,
-        "phone": phone,
-        "location": location,
-        "linkedin": linkedin,
-        "github": github,
-    }
-    info.append(info_dict)
-    response = {"id": len(info) - 1, "info": info_dict}
-    return response
+def create_info(
+    email: str,
+    phone: str,
+    location: str,
+    linkedin: str,
+    github: str,
+    session: SessionDep,
+):
+    info = Info(
+        email=email, phone=phone, location=location, linkedin=linkedin, github=github
+    )
+    session.add(info)
+    session.commit()
+    session.refresh(info)
+    return info
 
 
 @app.post("/projects")
-def create_project(name_project: str, description: str, link: str):
-    proj_dict = {"name_project": name_project, "description": description, "link": link}
-    projects.append(proj_dict)
-    response = {"id": len(projects) - 1, "project": proj_dict}
-    return response
+def create_project(name_project: str, description: str, link: str, session: SessionDep):
+    project = Project(name_project=name_project, description=description, link=link)
+    session.add(project)
+    session.commit()
+    session.refresh(project)
+    return project
 
 
 @app.get("/", response_class=HTMLResponse)
-def read_home(request: Request):
+def read_home(request: Request, session: SessionDep):
     return templates.TemplateResponse(
         request,
         "index.html",
         context={
-            "skills": skills,
-            "education": education,
-            "professional_experience": professional_experience,
-            "languages": languages,
-            "info": info,
-            "projects": projects,
+            "skills": session.exec(select(Skill)).all(),
+            "education": session.exec(select(Education)).all(),
+            "professional_experience": session.exec(
+                select(ProfessionalExperience)
+            ).all(),
+            "languages": session.exec(select(Language)).all(),
+            "info": session.exec(select(Info)).all(),
+            "projects": session.exec(select(Project)).all(),
         },
     )
 
 
-@app.delete("/skills/{skill_id}")
-def delete_skill(skill_id: int):
-    if 0 <= skill_id < len(skills):
-        deleted_skill = skills.pop(skill_id)
-        return {"message": "Skill deleted successfully", "deleted_skill": deleted_skill}
-    else:
-        return {"error": "Skill not found"}
+# @app.delete("/skills/{skill_id}")
+# def delete_skill(skill_id: int, session: SessionDep):
+# skill = session.get(Skill, skill_id)
+# if not skill:
+#   raise HTTPException(status_code=404, detail="Skill not found")
+# session.delete(skill)
+# session.commit()
+# return {"ok": True}
