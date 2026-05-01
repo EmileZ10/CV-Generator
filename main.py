ccc06a2533482710173773d67b65388b1070067a
EmileZ10 <emile.zanna@epfedu.fr>
2026-05-01 10:34:36 +0200

    fix: correction des erreurs lors de l'envoi de formulaires incomplets

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from fastapi import FastAPI, Request
-from pydantic import BaseModel
+from fastapi import FastAPI, Request, Form, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-
+from fastapi.staticfiles import StaticFiles
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from typing import Annotated
-from fastapi import Depends
-from fastapi.staticfiles import StaticFiles
 
 templates = Jinja2Templates(directory="templates")
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -27,50 +23,50 @@ SessionDep = Annotated[Session, Depends(get_session)]
 
 class Skill(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    software: str
-    level: str
+    software: str | None = None
+    level: str | None = None
 
 
 class Education(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    school: str
-    degree: str
-    field_of_study: str
-    start_year: int
-    end_year: int
+    school: str | None = None
+    degree: str | None = None
+    field_of_study: str | None = None
+    start_year: int | None = None
+    end_year: int | None = None
 
 
 class ProfessionalExperience(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    company: str
-    position: str
-    start_date: int
-    end_date: int
-    description: str
+    company: str | None = None
+    position: str | None = None
+    start_date: int | None = None
+    end_date: int | None = None
+    description: str | None = None
 
 
 class Language(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    language_name: str
-    level: str
+    language_name: str | None = None
+    level: str | None = None
 
 
 class Info(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    first_name: str
-    last_name: str
-    email: str
-    phone: str
-    location: str
-    linkedin: str
-    github: str
+    first_name: str | None = None
+    last_name: str | None = None
+    email: str | None = None
+    phone: str | None = None
+    location: str | None = None
+    linkedin: str | None = None
+    github: str | None = None
 
 
 class Project(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name_project: str
-    description: str
-    link: str
+    name_project: str | None = None
+    description: str | None = None
+    link: str | None = None
 
 
 @app.on_event("startup")
@@ -79,100 +75,107 @@ def on_startup():
 
 
 @app.post("/skills")
-def create_skill(software: str, level: str, session: SessionDep):
-    skill = Skill(software=software, level=level)
-    session.add(skill)
+def create_skill(
+    software: str | None = Form(None),
+    level: str | None = Form(None),
+    session: SessionDep = None,
+):
+    session.add(Skill(software=software, level=level))
     session.commit()
-    session.refresh(skill)
-    return RedirectResponse(url="/", status_code=303)
+    return RedirectResponse(url="/form", status_code=303)
 
 
 @app.post("/education")
 def create_education(
-    school: str,
-    degree: str,
-    field_of_study: str,
-    start_year: int,
-    end_year: int,
-    session: SessionDep,
+    school: str | None = Form(None),
+    degree: str | None = Form(None),
+    field_of_study: str | None = Form(None),
+    start_year: int | None = Form(None),
+    end_year: int | None = Form(None),
+    session: SessionDep = None,
 ):
-    education = Education(
-        school=school,
-        degree=degree,
-        field_of_study=field_of_study,
-        start_year=start_year,
-        end_year=end_year,
+    session.add(
+        Education(
+            school=school,
+            degree=degree,
+            field_of_study=field_of_study,
+            start_year=start_year,
+            end_year=end_year,
+        )
     )
-    session.add(education)
     session.commit()
-    session.refresh(education)
-    return RedirectResponse(url="/", status_code=303)
+    return RedirectResponse(url="/form", status_code=303)
 
 
 @app.post("/professional_experience")
 def create_professional_experience(
-    company: str,
-    position: str,
-    start_date: int,
-    end_date: int,
-    description: str,
-    session: SessionDep,
+    company: str | None = Form(None),
+    position: str | None = Form(None),
+    start_date: int | None = Form(None),
+    end_date: int | None = Form(None),
+    description: str | None = Form(None),
+    session: SessionDep = None,
 ):
-    experience = ProfessionalExperience(
-        company=company,
-        position=position,
-        start_date=start_date,
-        end_date=end_date,
-        description=description,
+    session.add(
+        ProfessionalExperience(
+            company=company,
+            position=position,
+            start_date=start_date,
+            end_date=end_date,
+            description=description,
+        )
     )
-    session.add(experience)
     session.commit()
-    session.refresh(experience)
-    return RedirectResponse(url="/", status_code=303)
+    return RedirectResponse(url="/form", status_code=303)
 
 
 @app.post("/languages")
-def create_language(language_name: str, level: str, session: SessionDep):
-    language = Language(language_name=language_name, level=level)
-    session.add(language)
+def create_language(
+    language_name: str | None = Form(None),
+    level: str | None = Form(None),
+    session: SessionDep = None,
+):
+    session.add(Language(language_name=language_name, level=level))
     session.commit()
-    session.refresh(language)
-    return RedirectResponse(url="/", status_code=303)
+    return RedirectResponse(url="/form", status_code=303)
 
 
 @app.post("/info")
 def create_info(
-    first_name: str,
-    last_name: str,
-    email: str,
-    phone: str,
-    location: str,
-    linkedin: str,
-    github: str,
-    session: SessionDep,
+    first_name: str | None = Form(None),
+    last_name: str | None = Form(None),
+    email: str | None = Form(None),
+    phone: str | None = Form(None),
+    location: str | None = Form(None),
+    linkedin: str | None = Form(None),
+    github: str | None = Form(None),
+    session: SessionDep = None,
 ):
-    info = Info(
-        first_name=first_name,
-        last_name=last_name,
-        email=email,
-        phone=phone,
-        location=location,
-        linkedin=linkedin,
-        github=github,
+    session.add(
+        Info(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            phone=phone,
+            location=location,
+            linkedin=linkedin,
+            github=github,
+        )
     )
-    session.add(info)
     session.commit()
-    session.refresh(info)
-    return RedirectResponse(url="/", status_code=303)
+    return RedirectResponse(url="/form", status_code=303)
 
 
 @app.post("/projects")
-def create_project(name_project: str, description: str, link: str, session: SessionDep):
-    project = Project(name_project=name_project, description=description, link=link)
-    session.add(project)
+def create_project(
+    name_project: str | None = Form(None),
+    description: str | None = Form(None),
+    link: str | None = Form(None),
+    session: SessionDep = None,
+):
+    session.add(Project(name_project=name_project, description=description, link=link))
     session.commit()
-    session.refresh(project)
-    return RedirectResponse(url="/", status_code=303)
+    return RedirectResponse(url="/form", status_code=303)
 
 
 @app.get("/form", response_class=HTMLResponse)
@@ -196,13 +199,3 @@ def read_home(request: Request, session: SessionDep):
             "projects": session.exec(select(Project)).all(),
         },
     )
-
-
-# @app.delete("/skills/{skill_id}")
-# def delete_skill(skill_id: int, session: SessionDep):
-# skill = session.get(Skill, skill_id)
-# if not skill:
-#   raise HTTPException(status_code=404, detail="Skill not found")
-# session.delete(skill)
-# session.commit()
-# return {"ok": True}
